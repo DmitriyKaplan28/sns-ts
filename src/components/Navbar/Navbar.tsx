3b009c80760072d93622fbc71bcd02f0e080fc17
DmitriyKaplan28 <dsk007@mail.ru>
2022-06-21 22:32:40 +0300

    74

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,7 +5,7 @@ import classes from './Navbar.module.css';
 export const Navbar = () => {
     return (
         <nav className={classes.nav}>
-            <div className={classes.item}><NavLink to="/profile/2" className={navData=>navData.isActive ? classes.active:classes.item}>Profile</NavLink></div>
+            <div className={classes.item}><NavLink to="/profile/24103" className={navData=>navData.isActive ? classes.active:classes.item}>Profile</NavLink></div>
             <div className={classes.item}><NavLink to="/dialogs" className={navData=>navData.isActive ? classes.active:classes.item}>Messages</NavLink></div>
             <div className={classes.item}><NavLink to="/users" className={navData=>navData.isActive ? classes.active:classes.item}>Users</NavLink></div>
             <div className={classes.item}><NavLink to="/news" className={navData=>navData.isActive ? classes.active:classes.item}>News</NavLink></div>
