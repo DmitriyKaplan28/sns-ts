7fda105ade5cdd6bc7b6d35894e0b3047ecc4925
DmitriyKaplan28 <dsk007@mail.ru>
2023-02-18 20:44:01 +0300

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -11,13 +11,10 @@ import {Provider} from "react-redux";
 ReactDOM.render(
     <BrowserRouter>
         <Provider store={store}>
-
             <App/>
         </Provider>
     </BrowserRouter>,
     document.getElementById('root')
 );
-
-
 
 reportWebVitals();
