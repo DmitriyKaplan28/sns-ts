15de660d08fbc8d7c1a5bbcc6d1c010be62b9e06
DmitriyKaplan28 <dsk007@mail.ru>
2023-04-24 17:07:55 +0300

    refactor: destructuring Dialogs props fix

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -7,7 +7,6 @@ import {Field, InjectedFormProps, reduxForm} from "redux-form";
 import {TextArea} from "../common/FormControls/FormControls";
 import {maxLengthCreator, requiredField} from "../../utils/validators/validators";
 
-/*export const Dialogs = (props: DialogsPropsType) => {*/
 export const Dialogs = ({dialogsState, sendMessage, isAuth}: DialogsPropsType) => {
 
     let dialogsElements = dialogsState.dialogs?.map((dialog, index) => <Dialog key={index}
