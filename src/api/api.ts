3825264f776f1b1f878b968e41c4027b4ceb6e1e
DmitriyKaplan28 <dsk007@mail.ru>
2023-07-31 20:59:14 +0300

    refactor: api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import {ProfileType} from "../components/Profile/ProfileContainer";
+import {ProfileType} from "../features/Profile/ProfileContainer";
 
 const instance = axios.create({
     withCredentials: true,
