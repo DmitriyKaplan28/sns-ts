03d67b16a3f335942be79fed3a07f7a88bbc0eaf
DmitriyKaplan28 <dsk007@mail.ru>
2023-08-18 17:36:11 +0300

    fix

--- src/features/Navbar/Navbar.tsx
+++ src/features/Navbar/Navbar.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {NavLink} from "react-router-dom";
 import classes from './Navbar.module.css';
 
+
 export const Navbar = () => {
     return (
         <nav className={classes.nav}>
