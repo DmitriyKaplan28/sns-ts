e69de58ba517be7505db384d76b04bba2a41fa98
DmitriyKaplan28 <dsk007@mail.ru>
2023-08-30 00:10:27 +0300

    refactor: Navbar.tsx in accordance with eslint config settings

--- src/features/Navbar/CustomNavLink/CustomNavLink.tsx
+++ src/features/Navbar/CustomNavLink/CustomNavLink.tsx
@@ -1,17 +1,16 @@
 import React from "react";
 import {NavLink} from "react-router-dom";
-import classes from './CustomNavLink.module.css';
+import classes from "./CustomNavLink.module.css";
 
 type CustomNavLinkPropsType = {
-    path: string
-    menuItem: string
+	path: string
+	menuItem: string
 }
 
-export const CustomNavLink = ({path, menuItem} : CustomNavLinkPropsType) => {
-    return (
-            <div className={classes.item}>
-                <NavLink to={path}
-                         className={navData => navData.isActive ? classes.active : classes.item}>{menuItem}</NavLink>
-            </div>
-    );
-}
+export const CustomNavLink = ({path, menuItem}: CustomNavLinkPropsType) => {
+	return (
+		<div className={classes.item}>
+			<NavLink to={path} className={navData => navData.isActive ? classes.active : classes.item}>{menuItem}</NavLink>
+		</div>
+	);
+};
--- src/features/Navbar/Navbar.tsx
+++ src/features/Navbar/Navbar.tsx
@@ -1,22 +1,22 @@
 import React from "react";
-import classes from './Navbar.module.css';
+import classes from "./Navbar.module.css";
 import {CustomNavLink} from "./CustomNavLink/CustomNavLink";
 
 
 export const Navbar = () => {
 
-    const menuItems = [
-        {path: "/profile/24103", menuItem: "Profile"},
-        {path: "/dialogs", menuItem: "Messages"},
-        {path: "/users", menuItem: "Users"},
-        {path: "/news", menuItem: "News"},
-        {path: "/music", menuItem: "Music"},
-        {path: "/settings", menuItem: "Settings"},
-    ]
+	const menuItems = [
+		{path: "/profile/24103", menuItem: "Profile"},
+		{path: "/dialogs", menuItem: "Messages"},
+		{path: "/users", menuItem: "Users"},
+		{path: "/news", menuItem: "News"},
+		{path: "/music", menuItem: "Music"},
+		{path: "/settings", menuItem: "Settings"},
+	];
 
-    return (
-        <nav className={classes.nav}>
-            {menuItems.map(m => <CustomNavLink path={m.path} menuItem={m.menuItem} />)}
-        </nav>
-    );
-}
+	return (
+		<nav className={classes.nav}>
+			{menuItems.map(m => <CustomNavLink path={m.path} menuItem={m.menuItem}/>)}
+		</nav>
+	);
+};
