725b7fc04ae512ba1824493dea2640b7e9207308
DmitriyKaplan28 <dsk007@mail.ru>
2023-08-30 23:40:34 +0300

    refactor: Contact.tsx in accordance with eslint config settings

--- src/features/Profile/ProfileInfo/Contacts/Contact.tsx
+++ src/features/Profile/ProfileInfo/Contacts/Contact.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import styles from './Contact.module.css'
+import React from "react";
+import styles from "./Contact.module.css";
 
 type contactPropsType = {
-    title: string
-    value: string
+	title: string
+	value: string
 }
 
 export const Contact = ({title, value}: contactPropsType) => {
-    return (
-        <div className={styles.contact}>
-            <b>{title}</b>:{value}
-        </div>
-    );
+	return (
+		<div className={styles.contact}>
+			<b>{title}</b>:{value}
+		</div>
+	);
 };
