62b0ae85ab28b1b0c6a3e35e15f79c026e6cdb78
DmitriyKaplan28 <dsk007@mail.ru>
2023-08-30 23:39:50 +0300

    refactor: ProfileData.tsx in accordance with eslint config settings

--- src/features/Profile/ProfileInfo/ProfileData/ProfileData.tsx
+++ src/features/Profile/ProfileInfo/ProfileData/ProfileData.tsx
@@ -1,31 +1,31 @@
-import React from 'react';
+import React from "react";
 import {ProfileType} from "../../ProfileContainer";
 
 type ProfileDataType = {
-    profile: ProfileType
-    isOwner: boolean
-    editModeOn: () => void
+	profile: ProfileType
+	isOwner: boolean
+	editModeOn: () => void
 }
 
 export const ProfileData = ({profile, isOwner, editModeOn}: ProfileDataType) => {
-    return (
-        <div>
-            {isOwner && <button onClick={editModeOn}>Edit profile</button>}
-            <div>
-                <b>Full name</b>: {profile.fullName}
-            </div>
+	return (
+		<div>
+			{isOwner && <button onClick={editModeOn}>Edit profile</button>}
+			<div>
+				<b>Full name</b>: {profile.fullName}
+			</div>
 
-            <div>
-                <b>Looking for a job</b>: {profile.lookingForAJob ? "Yes" : "No"}
-            </div>
-            {profile.lookingForAJob &&
-                <div>
-                    <b>My professional skills</b>{profile.lookingForAJobDescription}
-                </div>
-            }
-            <div>
-                <b>About me</b>: {profile.aboutMe}
-            </div>
-        </div>
-    );
+			<div>
+				<b>Looking for a job</b>: {profile.lookingForAJob ? "Yes" : "No"}
+			</div>
+			{profile.lookingForAJob &&
+				<div>
+					<b>My professional skills</b>{profile.lookingForAJobDescription}
+				</div>
+			}
+			<div>
+				<b>About me</b>: {profile.aboutMe}
+			</div>
+		</div>
+	);
 };
