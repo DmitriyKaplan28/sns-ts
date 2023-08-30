7e5eef5f5fc4701d71e3a073bbb794a2cd2b81e5
DmitriyKaplan28 <dsk007@mail.ru>
2023-08-31 00:09:37 +0300

    refactor: MyPosts in accordance with eslint config settings

--- src/features/Profile/MyPosts/MyPosts.tsx
+++ src/features/Profile/MyPosts/MyPosts.tsx
@@ -1,57 +1,60 @@
 import React from "react";
-import classes from './MyPosts.module.css';
+import classes from "./MyPosts.module.css";
 import {Post, PostPropsType} from "./Post/Post";
 import {Field, InjectedFormProps, reduxForm} from "redux-form";
-import {maxLengthCreator, requiredField} from "../../../common/utils/validators/validators";
+import {
+	maxLengthCreator,
+	requiredField
+} from "../../../common/utils/validators/validators";
 import {TextArea} from "../../../common/components/FormControls/FormControls";
 
 type MyPostsType = {
-    posts: PostPropsType[]
-    newPostText: string
-    addPost: (newPostText: string) => void
-    updateNewPostText: (newText: string) => void
+	posts: PostPropsType[]
+	newPostText: string
+	addPost: (newPostText: string) => void
+	updateNewPostText: (newText: string) => void
 }
 
 type FormDataType = {
-    newPostText: string
+	newPostText: string
 }
 
 export const MyPosts = React.memo((props: MyPostsType) => {
 
-    let postsElements = props?.posts?.map((post) =>
-        <Post post={post.post} like={post.like}/>
-    )
+	const postsElements = props?.posts?.map((post) =>
+		<Post post={post.post} like={post.like}/>
+	);
 
-    const addPost = (formData: FormDataType) => {
-        props.addPost(formData.newPostText)
-    }
+	const addPost = (formData: FormDataType) => {
+		props.addPost(formData.newPostText);
+	};
 
-    return (
+	return (
 
-        <div className={classes.postsBlock}>
-            <h3>My posts</h3>
-            <div>
-                <AddPostReduxForm onSubmit={addPost}/>
-            </div>
-            <div className={classes.post}>
-                {postsElements}
-            </div>
-        </div>
-    );
-})
+		<div className={classes.postsBlock}>
+			<h3>My posts</h3>
+			<div>
+				<AddPostReduxForm onSubmit={addPost}/>
+			</div>
+			<div className={classes.post}>
+				{postsElements}
+			</div>
+		</div>
+	);
+});
 
-const maxLength10 = maxLengthCreator(10)
+const maxLength10 = maxLengthCreator(10);
 
 const AddPostForm: React.FC<InjectedFormProps<FormDataType>> = (props) => {
-    return (
-        <form onSubmit={props.handleSubmit}>
-            <Field placeholder={'Enter your post'}
-                   name={'newPostText'}
-                   component={TextArea}
-                   validate={[requiredField, maxLength10]}
-            />
-            <button>Add Post</button>
-        </form>)
-}
+	return (
+		<form onSubmit={props.handleSubmit}>
+			<Field placeholder={"Enter your post"}
+				   name={"newPostText"}
+				   component={TextArea}
+				   validate={[requiredField, maxLength10]}
+			/>
+			<button>Add Post</button>
+		</form>);
+};
 
-const AddPostReduxForm = reduxForm<FormDataType>({form: 'addPostForm'})(AddPostForm)
+const AddPostReduxForm = reduxForm<FormDataType>({form: "addPostForm"})(AddPostForm);
--- src/features/Profile/MyPosts/MyPostsContainer.tsx
+++ src/features/Profile/MyPosts/MyPostsContainer.tsx
@@ -7,32 +7,32 @@ import {PostPropsType} from "./Post/Post";
 import {Dispatch} from "redux";
 
 type MapStateToPropsMyPostsType = {
-    posts: PostPropsType[]
-    newPostText: string
+	posts: PostPropsType[]
+	newPostText: string
 }
 
 type mapDispatchToPropsMyPostsType = {
-    updateNewPostText: (newText: string) => void
-    addPost: (newPostText: string) => void
+	updateNewPostText: (newText: string) => void
+	addPost: (newPostText: string) => void
 }
 
-let mapStateToProps = (state: AppStateType): MapStateToPropsMyPostsType => {
-    return {
-        posts: state.profilePage.posts,
-        newPostText: state.profilePage.newPostText,
-    }
-}
-let mapDispatchToProps = (dispatch: Dispatch): mapDispatchToPropsMyPostsType => {
-    return {
-        updateNewPostText: (newText: string) => {
-            let action = updateNewPostTextAC(newText)
-            dispatch(action)
-        },
-        addPost: (newPostText: string) => {
-            dispatch(addPostAC(newPostText))
-        },
+const mapStateToProps = (state: AppStateType): MapStateToPropsMyPostsType => {
+	return {
+		posts: state.profilePage.posts,
+		newPostText: state.profilePage.newPostText,
+	};
+};
+const mapDispatchToProps = (dispatch: Dispatch): mapDispatchToPropsMyPostsType => {
+	return {
+		updateNewPostText: (newText: string) => {
+			const action = updateNewPostTextAC(newText);
+			dispatch(action);
+		},
+		addPost: (newPostText: string) => {
+			dispatch(addPostAC(newPostText));
+		},
 
-    }
-}
+	};
+};
 
 export const MyPostsContainer = connect(mapStateToProps, mapDispatchToProps)(MyPosts);
