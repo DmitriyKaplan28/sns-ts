0c5408a1b48ab1eebe0607b7cf63a5e6ce2bffc0
DmitriyKaplan28 <dsk007@mail.ru>
2023-08-30 23:35:08 +0300

    refactor: ProfileStatus.tsx in accordance with eslint config settings

--- src/features/Profile/ProfileInfo/ProfileStatus/ProfileStatus.test.tsx
+++ src/features/Profile/ProfileInfo/ProfileStatus/ProfileStatus.test.tsx
@@ -1,42 +1,43 @@
-import TestRenderer from 'react-test-renderer';
+import React from "react";
+import TestRenderer from "react-test-renderer";
 import {
-    ProfileStatus
+	ProfileStatus
 } from "./ProfileStatus";
 
 
 describe("ProfileStatus component", () => {
-    test("span should be displayed", () => {
-        const testRenderer = TestRenderer.create(<ProfileStatus
-            status="test status" updateUserStatus={() => {
-        }}/>);
-        const testInstance = testRenderer.root;
+	test("span should be displayed", () => {
+		const testRenderer = TestRenderer.create(<ProfileStatus
+			status="test status" updateUserStatus={() => {
+			}}/>);
+		const testInstance = testRenderer.root;
 
-        expect(testInstance.findByType("span"));
-    });
-    test("input should not be displayed", () => {
-        const testRenderer = TestRenderer.create(<ProfileStatus
-            status="test status" updateUserStatus={() => {
-        }}/>);
-        const testInstance = testRenderer.root;
+		expect(testInstance.findByType("span"));
+	});
+	test("input should not be displayed", () => {
+		const testRenderer = TestRenderer.create(<ProfileStatus
+			status="test status" updateUserStatus={() => {
+			}}/>);
+		const testInstance = testRenderer.root;
 
-        expect(() => {
-            testInstance.findByType("input")
-        }).toThrow();
-    });
-    test("status is correct", () => {
-        const testRenderer = TestRenderer.create(<ProfileStatus
-            status="test status" updateUserStatus={() => {
-        }}/>);
-        const testInstance = testRenderer.root;
-        expect(testInstance.findByType("span").children[0]).toBe("test status");
-    });
-    test("input should be displayed in edit mode", () => {
-        const testRenderer = TestRenderer.create(<ProfileStatus
-            status="test status" updateUserStatus={() => {
-        }}/>);
-        const testInstance = testRenderer.root;
-        let span = testInstance.findByType("span")
-        span.props.onDoubleClick();
-        expect(testInstance.findByType("input"));
-    })
-})
+		expect(() => {
+			testInstance.findByType("input");
+		}).toThrow();
+	});
+	test("status is correct", () => {
+		const testRenderer = TestRenderer.create(<ProfileStatus
+			status="test status" updateUserStatus={() => {
+			}}/>);
+		const testInstance = testRenderer.root;
+		expect(testInstance.findByType("span").children[0]).toBe("test status");
+	});
+	test("input should be displayed in edit mode", () => {
+		const testRenderer = TestRenderer.create(<ProfileStatus
+			status="test status" updateUserStatus={() => {
+			}}/>);
+		const testInstance = testRenderer.root;
+		const span = testInstance.findByType("span");
+		span.props.onDoubleClick();
+		expect(testInstance.findByType("input"));
+	});
+});
--- src/features/Profile/ProfileInfo/ProfileStatus/ProfileStatus.tsx
+++ src/features/Profile/ProfileInfo/ProfileStatus/ProfileStatus.tsx
@@ -2,40 +2,40 @@ import React, {ChangeEvent, useEffect, useState} from "react";
 
 
 type ProfileStatusType = {
-    status: string
-    updateUserStatus: (status: string) => void
+	status: string
+	updateUserStatus: (status: string) => void
 }
 
 export const ProfileStatus = ({
-                                  status,
-                                  updateUserStatus
-                              }: ProfileStatusType) => {
-    const [editMode, setEditMode] = useState<boolean>(false)
-    const [profileStatus, setProfileStatus] = useState<string>(status)
+	status,
+	updateUserStatus
+}: ProfileStatusType) => {
+	const [editMode, setEditMode] = useState<boolean>(false);
+	const [profileStatus, setProfileStatus] = useState<string>(status);
 
-    useEffect(() => {
-        setProfileStatus(status)
-    }, [status])
+	useEffect(() => {
+		setProfileStatus(status);
+	}, [status]);
 
-    const editModeChange = () => {
-        setEditMode(!editMode)
-    }
+	const editModeChange = () => {
+		setEditMode(!editMode);
+	};
 
-    const onStatusChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setProfileStatus(e.currentTarget.value)
-        updateUserStatus(profileStatus)
-    }
+	const onStatusChange = (e: ChangeEvent<HTMLInputElement>) => {
+		setProfileStatus(e.currentTarget.value);
+		updateUserStatus(profileStatus);
+	};
 
-    return (
-        <div>
-            {!editMode && <div>
-                <span onDoubleClick={editModeChange}>{status || '----'}</span>
-            </div>}
-            {editMode &&
-                <div>
-                    <input onChange={onStatusChange} onBlur={editModeChange}
-                           value={profileStatus}/>
-                </div>}
-        </div>
-    );
-}
+	return (
+		<div>
+			{!editMode && <div>
+				<span onDoubleClick={editModeChange}>{status || "----"}</span>
+			</div>}
+			{editMode &&
+				<div>
+					<input onChange={onStatusChange} onBlur={editModeChange}
+						   value={profileStatus}/>
+				</div>}
+		</div>
+	);
+};
