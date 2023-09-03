e3c6f0a66c35b2ae47a8980b253b80118dae77ab
DmitriyKaplan28 <dsk007@mail.ru>
2023-09-03 21:51:38 +0300

    refactor: Dialogs in accordance with eslint config settings

--- src/features/Dialogs/Dialogs.tsx
+++ src/features/Dialogs/Dialogs.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import classes from './Dialogs.module.css';
+import React from "react";
+import classes from "./Dialogs.module.css";
 import {Dialog} from "./Dialog/Dialog";
 import {Message} from "./Message/Message";
 import {DialogsPropsType} from "./DialogsContainer";
@@ -9,50 +9,50 @@ import {maxLengthCreator, requiredField} from "../../common/utils/validators/val
 
 
 type FormDataType = {
-    newMessageBody: string
+	newMessageBody: string
 }
 
 export const Dialogs = ({dialogsState, sendMessage, isAuth}: DialogsPropsType) => {
 
-    let dialogsElements = dialogsState.dialogs?.map((dialog, index) => <Dialog key={index}
-                                                                               name={dialog.name}
-                                                                               id={dialog.id}/>)
-    let messagesElements = dialogsState.messages?.map((message, index) => <Message
-        key={index}
-        message={message.message}/>)
-    const addNewMessage = (formData: FormDataType) => {
-        sendMessage(formData.newMessageBody)
-    }
+	const dialogsElements = dialogsState.dialogs?.map((dialog, index) => <Dialog key={index}
+																			   name={dialog.name}
+																			   id={dialog.id}/>);
+	const messagesElements = dialogsState.messages?.map((message, index) => <Message
+		key={index}
+		message={message.message}/>);
+	const addNewMessage = (formData: FormDataType) => {
+		sendMessage(formData.newMessageBody);
+	};
 
-    return (
-        <div className={classes.dialogs}>
-            <div className={classes.dialogsItems}>
-                {dialogsElements}
-            </div>
-            <div className={classes.messages}>
-                <div>{messagesElements}</div>
-                <div>
-                    <AddMessageReduxForm onSubmit={addNewMessage}/>
-                </div>
-            </div>
-        </div>
-    )
-}
+	return (
+		<div className={classes.dialogs}>
+			<div className={classes.dialogsItems}>
+				{dialogsElements}
+			</div>
+			<div className={classes.messages}>
+				<div>{messagesElements}</div>
+				<div>
+					<AddMessageReduxForm onSubmit={addNewMessage}/>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 
-const maxLength100 = maxLengthCreator(100)
+const maxLength100 = maxLengthCreator(100);
 
 const AddMessageForm: React.FC<InjectedFormProps<FormDataType>> = (props) => {
 
-    return (
-        <form onSubmit={props.handleSubmit}>
-            <Field placeholder={'Enter your message'}
-                   name={'newMessageBody'}
-                   component={TextArea}
-                   validate={[requiredField, maxLength100]}
-            />
-            <button>Send</button>
-        </form>)
-}
+	return (
+		<form onSubmit={props.handleSubmit}>
+			<Field placeholder={"Enter your message"}
+				   name={"newMessageBody"}
+				   component={TextArea}
+				   validate={[requiredField, maxLength100]}
+			/>
+			<button>Send</button>
+		</form>);
+};
 
-const AddMessageReduxForm = reduxForm<FormDataType>({form: 'addMessageForm'})(AddMessageForm)
+const AddMessageReduxForm = reduxForm<FormDataType>({form: "addMessageForm"})(AddMessageForm);
--- src/features/Dialogs/DialogsContainer.tsx
+++ src/features/Dialogs/DialogsContainer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {DialogType} from "./Dialog/Dialog";
 import {MessageType} from "./Message/Message";
 import {sendMessageAC} from "../../store/dialogsReducer";
@@ -8,34 +8,34 @@ import {AppStateType} from "../../store/redux-store";
 import {compose, Dispatch} from "redux";
 
 export type DialogsStateType = {
-    dialogs: DialogType[]
-    messages: MessageType[]
+	dialogs: DialogType[]
+	messages: MessageType[]
 }
 
 type MapStateToPropsDialogsType = {
-    dialogsState: DialogsStateType
-    isAuth: boolean
+	dialogsState: DialogsStateType
+	isAuth: boolean
 }
 
 type mapDispatchToPropsDialogsType = {
-    sendMessage: (newMessageBody: string) => void
+	sendMessage: (newMessageBody: string) => void
 }
 
 export type DialogsPropsType = MapStateToPropsDialogsType & mapDispatchToPropsDialogsType
 
-let mapStateToProps = (state: AppStateType): MapStateToPropsDialogsType => {
-    return {
-        dialogsState: state.dialogsPage,
-        isAuth: state.auth.isAuth
-    }
-}
+const mapStateToProps = (state: AppStateType): MapStateToPropsDialogsType => {
+	return {
+		dialogsState: state.dialogsPage,
+		isAuth: state.auth.isAuth
+	};
+};
 
-let mapDispatchToProps = (dispatch: Dispatch): mapDispatchToPropsDialogsType => {
-    return {
-        sendMessage: (newMessageBody: string) => {
-            dispatch(sendMessageAC(newMessageBody))
-        },
-    }
-}
+const mapDispatchToProps = (dispatch: Dispatch): mapDispatchToPropsDialogsType => {
+	return {
+		sendMessage: (newMessageBody: string) => {
+			dispatch(sendMessageAC(newMessageBody));
+		},
+	};
+};
 
-export const DialogsContainer = compose<React.ComponentType>(connect(mapStateToProps, mapDispatchToProps))(Dialogs)
+export const DialogsContainer = compose<React.ComponentType>(connect(mapStateToProps, mapDispatchToProps))(Dialogs);
