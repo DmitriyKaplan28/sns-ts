bb65d1699a2b6425c5411322112afa230a8e450c
DmitriyKaplan28 <dsk007@mail.ru>
2023-09-03 21:52:07 +0300

    refactor: Message in accordance with eslint config settings

--- src/features/Dialogs/Message/Message.tsx
+++ src/features/Dialogs/Message/Message.tsx
@@ -2,13 +2,13 @@ import classes from "../Dialogs.module.css";
 import React from "react";
 
 export type MessageType = {
-    message: string
-    id?: string
+	message: string
+	id?: string
 }
 
 
 export const Message = ({message, id}: MessageType) => {
-    return (
-        <div className={classes.message}>{message}</div>
-    )
-}
+	return (
+		<div className={classes.message}>{message}</div>
+	);
+};
