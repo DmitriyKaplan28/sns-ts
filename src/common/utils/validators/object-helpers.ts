13a2bd3a6bdf9a2dedc61c77d6b21192506531e8
DmitriyKaplan28 <dsk007@mail.ru>
2023-09-04 16:41:39 +0300

    refactor: utils in accordance with eslint config settings

--- src/common/utils/validators/object-helpers.ts
+++ src/common/utils/validators/object-helpers.ts
@@ -2,12 +2,12 @@ import {UserType} from "../../../store/usersReducer";
 
 
 export const updateObjectInArray = (items: UserType[], itemId: number, objUserPropName: keyof UserType,
-                                    newUserObjProps: { followed: boolean }): UserType[] => {
-    return items.map(i => {
-            if (i[objUserPropName] === itemId) {
-                return {...i, ...newUserObjProps}
-            }
-            return i;
-        }
-    )
-}
+	newUserObjProps: { followed: boolean }): UserType[] => {
+	return items.map(i => {
+		if (i[objUserPropName] === itemId) {
+			return {...i, ...newUserObjProps};
+		}
+		return i;
+	}
+	);
+};
