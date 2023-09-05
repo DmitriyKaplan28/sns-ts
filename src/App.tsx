db3d7d162d4c5eb2562d2aa2258d7231bf40f8bd
DmitriyKaplan28 <dsk007@mail.ru>
2023-09-05 21:23:50 +0300

    refactor: App in accordance with eslint config settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, {lazy, Suspense} from 'react';
-import './App.css';
+import React, {lazy, Suspense} from "react";
+import "./App.css";
 import {Navbar} from "./features/Navbar/Navbar";
 import {Routes, Route, BrowserRouter, Navigate} from "react-router-dom";
 import {UsersContainer} from "./features/Users/UsersContainer";
@@ -14,87 +14,87 @@ import {Preloader} from "./common/components/Preloader/Preloader";
 
 
 const DialogsContainer = lazy(() =>
-    import('./features/Dialogs/DialogsContainer')
-        .then(({DialogsContainer}) => ({default: DialogsContainer})),
+	import("./features/Dialogs/DialogsContainer")
+		.then(({DialogsContainer}) => ({default: DialogsContainer})),
 );
 
 const ProfileContainer = lazy(() =>
-    import('./features/Profile/ProfileContainer')
-        .then(({ProfileContainer}) => ({default: ProfileContainer})),
+	import("./features/Profile/ProfileContainer")
+		.then(({ProfileContainer}) => ({default: ProfileContainer})),
 );
 
 type MapStateToPropsType = {
-    initialized: boolean
+	initialized: boolean
 }
 
 type MapDispatchToPropsType = {
-    initializeTC: () => void
+	initializeTC: () => void
 }
 
 type AppType = MapDispatchToPropsType & MapStateToPropsType
 
 class App extends React.Component<AppType> {
 
-    handleError = (promiseRejectionEvent: any) => {
-        alert('Error');
-        console.error(promiseRejectionEvent)
+	handleError = (promiseRejectionEvent: any) => {
+		alert("Error");
+		console.error(promiseRejectionEvent);
 
-    }
+	};
 
-    componentDidMount() {
-        this.props.initializeTC()
-        window.addEventListener("unhandledrejection", this.handleError)
-    }
+	componentDidMount() {
+		this.props.initializeTC();
+		window.addEventListener("unhandledrejection", this.handleError);
+	}
 
-    componentWillUnmount() {
-        window.removeEventListener("unhandledrejection", this.handleError)
-    }
+	componentWillUnmount() {
+		window.removeEventListener("unhandledrejection", this.handleError);
+	}
 
 
-    render() {
-        if (!this.props.initialized) {
-            return <Preloader/>
-        }
+	render() {
+		if (!this.props.initialized) {
+			return <Preloader/>;
+		}
 
-        return (
-            <div className='app-wrapper'>
-                <HeaderContainer/>
-                <Navbar/>
-                <div className='app-wrapper-content'>
-                    <Routes>
-                        <Route path='/dialogs' element={
-                            <Suspense fallback={<Preloader/>}>
-                                <DialogsContainer/>
-                            </Suspense>
-                        }/>
-                        <Route path='/profile/:userId' element={
-                            <Suspense fallback={<Preloader/>}>
-                                <ProfileContainer/>
-                            </Suspense>
-                        }/>
-                        {/*<Route path='/profile' element={<ProfileContainer/>}/>*/}
-                        <Route path='/users' element={<UsersContainer/>}/>
-                        <Route path='/login' element={<Login/>}/>
-                        <Route path='/*' element={<div>404 NOT FOUND</div>}/>
-                        <Route path='/'
-                               element={<Navigate to="/profile/:userId" replace/>}/>
-                    </Routes>
-                </div>
-            </div>
-        );
-    }
+		return (
+			<div className="app-wrapper">
+				<HeaderContainer/>
+				<Navbar/>
+				<div className="app-wrapper-content">
+					<Routes>
+						<Route path="/dialogs" element={
+							<Suspense fallback={<Preloader/>}>
+								<DialogsContainer/>
+							</Suspense>
+						}/>
+						<Route path="/profile/:userId" element={
+							<Suspense fallback={<Preloader/>}>
+								<ProfileContainer/>
+							</Suspense>
+						}/>
+						{/*<Route path='/profile' element={<ProfileContainer/>}/>*/}
+						<Route path="/users" element={<UsersContainer/>}/>
+						<Route path="/login" element={<Login/>}/>
+						<Route path="/*" element={<div>404 NOT FOUND</div>}/>
+						<Route path="/"
+							   element={<Navigate to="/profile/:userId" replace/>}/>
+					</Routes>
+				</div>
+			</div>
+		);
+	}
 }
 
 const mapStateToProps = (state: AppStateType): MapStateToPropsType => ({
-    initialized: state.app.initialized
-})
+	initialized: state.app.initialized
+});
 
 const AppContainer = compose<React.ComponentType>(connect(mapStateToProps, {initializeTC}), WithRouter)(App);
 
 export const SamuraiJSApp = () => {
-    return <BrowserRouter>
-        <Provider store={store}>
-            <AppContainer/>
-        </Provider>
-    </BrowserRouter>
-}
+	return <BrowserRouter>
+		<Provider store={store}>
+			<AppContainer/>
+		</Provider>
+	</BrowserRouter>;
+};
